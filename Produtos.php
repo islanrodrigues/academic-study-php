3d19cdfa9fc74a400f97250e5c6299e1b6b64c18
Islan Rodrigues <jose-islan52@hotmail.com>
2019-02-23 14:11:35 -0300

    Criação das classes entidades 'Usuarios' e 'Produtos'

---
+++ Produtos.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Description of Produtos
+ *
+ * @author Galvão
+ */
+class Produtos {
+    
+    private $productID, $productName, $supplierID, $categoryID, $quantityPerUnit, $uniPrice;
+    
+    
+      // GETTERS E SETTERS
+    function getProductID() {
+        return $this->productID;
+    }
+    
+    function setProductID($productID) {
+        $this->productID = $productID;
+    }
+    
+
+    function getProductName() {
+        return $this->productName;
+    }
+    
+    function setProductName($productName) {
+        $this->productName = $productName;
+    }
+    
+
+    function getSupplierID() {
+        return $this->supplierID;
+    }
+    
+    function setSupplierID($supplierID) {
+        $this->supplierID = $supplierID;
+    }
+    
+
+    function getCategoryID() {
+        return $this->categoryID;
+    }
+    
+    function setCategoryID($categoryID) {
+        $this->categoryID = $categoryID;
+    }
+    
+
+    function getQuantityPerUnit() {
+        return $this->quantityPerUnit;
+    }
+    
+    function setQuantityPerUnit($quantityPerUnit) {
+        $this->quantityPerUnit = $quantityPerUnit;
+    }
+    
+
+    function getUniPrice() {
+        return $this->uniPrice;
+    }    
+
+    function setUniPrice($uniPrice) {
+        $this->uniPrice = $uniPrice;
+    }
+    
+}
+
+?>
