507562a9a540bd52c69a15ae5ef8441b4d082834
Islan Rodrigues <jose-islan52@hotmail.com>
2019-05-15 18:40:44 -0300

    Finalizando as telas de cadastro e detalhamento de Fornecedor, juntamente com os métodos de edição e exclusão

---
+++ view/detalhesFornecedorView.php
@@ -0,0 +1,165 @@
+<?php
+    require_once '../dao/ConexaoDAO.php';     
+    require_once '../dao/FornecedoresDAO.php';
+        
+    $fornDAO = new FornecedoresDAO();
+    
+    if (isset($_GET['id'])) {
+        
+        if (isset($_GET['delete']) && $_GET['delete'] == 1) {
+            $acao = 1;
+        } else {
+            $acao = null;
+        }
+        
+        if ($acao != NULL) {
+            
+            $fornDAO->deletarFornecedor($_GET['id']);
+            
+           echo "<script>alert('Fornecedor excluído com sucesso!')</script>";
+           echo "<script>location.href='listarFornecedoresView.php'</script>";
+        }
+        
+         $fornecedor = $fornDAO->visualizarFornecedor($_GET['id']);
+         
+        
+    } else {
+        $produto = NULL;
+    }
+    
+?>
+
+<html>
+
+    <head>
+	<meta charset="utf-8">
+        <title>Detalhes do Fornecedor</title>
+	<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../assets/css/all.css">
+        <script type="text/javascript" src="../assets/js/jquery-3.3.1.min.js"></script>
+        
+    </head>
+    
+    <script type="text/javascript">
+        
+        function deleteItem(url) {
+            if (confirm("Tem certeza que deseja excluir este Fornecedor?")) {
+                document.location = url;
+            }
+        }
+    
+    </script>
+
+    <body>
+
+	<div class = "container-fluid" id="div-main">
+            
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#"> CIÊNCIA DA COMPUTAÇÃO - 7 CIC</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navForn" aria-controls="navForn" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            
+                <div class="collapse navbar-collapse" id="navForn">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Produtos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Fornecedores</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Categorias</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Clientes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Funcionários</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        
+            <hr>
+            
+            <div class="row">
+                
+                <?php 
+                    
+                    if ($fornecedor == NULL) {
+                        
+                ?>
+                
+                <h2><center>FORNECEDOR NÃO ENCONTRADO!<i class="far fa-frown" style="margin-left: 10px"></i></center></h2>
+                
+                <?php
+                    
+                    } else {
+                        
+                ?>
+                
+                <div class="table-responsive col-7">
+                    <table class="table table-striped">                        
+                        <tbody>
+                            <tr>
+                                <th>Código: <?= $fornecedor->getSupplierID() ?></th>
+                            </tr>
+                            
+                            <tr>
+                                <th>Nome da Companhia: <?= $fornecedor->getCompanyName() ?></th>
+                            </tr>
+                            
+                           <tr>
+                               <th>Nome do Contato: <?= $fornecedor->getContactName() ?></th>
+                            </tr> 
+                            
+                            <tr>
+                                <th>Título do Contato: <?= $fornecedor->getContactTitle() ?></th>
+                            </tr>
+                            
+                            <tr>
+                                <th>Endereço: <?= $fornecedor->getAddress() ?></th>
+                            </tr>
+                            <tr>
+                                <th>Cidade: <?= $fornecedor->getCity() ?></th>
+                            </tr>
+                            <tr>
+                                <th>Região: <?= $fornecedor->getRegion() ?></th>
+                            </tr>
+                            <tr>
+                                <th>Caixa Postal: <?= $fornecedor->getPostalCode() ?></th>
+                            </tr>
+                            <tr>
+                                <th>País: <?= $fornecedor->getCountry() ?></th>
+                            </tr>
+                            <tr>
+                                <th>Telefone: <?= $fornecedor->getPhone() ?></th>
+                            </tr>
+                            <tr>
+                                <th><center>
+                                    <a href="formFornecedorView.php?id=<?= $_GET['id'] ?>&dados=1" class="btn btn-primary">Editar Fornecedor <i class="fas fa-pencil-alt" style="margin-left: 5px"></i></a>
+                                    <a href="javascript:deleteItem('detalhesFornecedorView.php?id=<?= $_GET['id'] ?>&delete=1')" class="btn btn-danger">Excluir Fornecedor<i class="fas fa-trash-alt" style="margin-left: 5px"></i></a>
+                                </center></th>
+                            </tr>
+                        </tbody>
+                    </table>
+                
+                <?php
+                        
+                    }          
+                
+                ?>
+                
+                
+                </div>
+                
+            </div>
+        
+        </div>
+        
+    </body>
+    
+</html>
+        
+
---
+++ view/formFornecedorView.php
@@ -0,0 +1,315 @@
+<?php
+
+    require_once '../dao/FornecedoresDAO.php';
+    require_once '../model/Produtos.php';
+    
+    $fornDAO = new FornecedoresDAO();
+    
+    if (isset($_GET['acao'])) {
+        $acao = $_GET['acao'];
+        
+        $objForn = new Fornecedores();
+      
+        //CADASTRAR FORNECEDOR
+        if ($acao == 1) {
+            $fornecedores = $fornDAO->listarFornecedores(0, null);
+            $lastForn = $fornecedores[count($fornecedores) - 1];
+            $fornID = $lastForn->getSupplierID();
+            $fornID++;
+            
+            $nomeComp = $_GET['company_forn'];
+            $nomeCont = $_GET['contato_forn'];
+            $titutoCont = $_GET['titulo_forn'];
+            $endereco = $_GET['endereco_forn'];
+            $cidade = $_GET['cidade_forn'];
+            $regiao = $_GET['regiao_forn'];
+            $postal = $_GET['postal_forn'];
+            $pais = $_GET['pais_forn'];
+            $telefone = $_GET['tel_forn'];
+            
+            $objForn->setSupplierID($fornID);
+            $objForn->setCompanyName($nomeComp);
+            $objForn->setContactName($nomeCont);
+            $objForn->setContactTitle($titutoCont);
+            $objForn->setAddress($endereco);
+            $objForn->setCity($cidade);
+            $objForn->setRegion($regiao);
+            $objForn->setPostalCode($postal);
+            $objForn->setCountry($pais);
+            $objForn->setPhone($telefone);
+            
+            $fornDAO->cadastrarFornecedor($objForn);
+            
+            echo "<script>alert('Fornecedor $nomeComp cadastrado com sucesso!')</script>";
+            echo "<script>location.href='listarFornecedoresView.php'</script>";            
+        
+        //ATUALIZAR FORNECEDOR
+        } else if ($acao == 2) {
+            $id = $_GET['id'];
+            $nomeComp = $_GET['company_forn'];
+            $nomeCont = $_GET['contato_forn'];
+            $titutoCont = $_GET['titulo_forn'];
+            $endereco = $_GET['endereco_forn'];
+            $cidade = $_GET['cidade_forn'];
+            $regiao = $_GET['regiao_forn'];
+            $postal = $_GET['postal_forn'];
+            $pais = $_GET['pais_forn'];
+            $telefone = $_GET['tel_forn'];
+            
+            $objForn->setSupplierID($id);
+            $objForn->setCompanyName($nomeComp);
+            $objForn->setContactName($nomeCont);
+            $objForn->setContactTitle($titutoCont);
+            $objForn->setAddress($endereco);
+            $objForn->setCity($cidade);
+            $objForn->setRegion($regiao);
+            $objForn->setPostalCode($postal);
+            $objForn->setCountry($pais);
+            $objForn->setPhone($telefone);
+            
+            $fornDAO->alterarFornecedor($id, $objForn);
+            
+            echo "<script>alert('Fornecedor $nomeComp alterado com sucesso!')</script>";
+            echo "<script>location.href='listarFornecedoresView.php'</script>"; 
+        }
+        
+    } else {
+        
+        if (!isset($_GET['dados'])) {
+           $dados = null; 
+           
+        } else {
+            $dados = $_GET['dados'];
+        }       
+        
+             
+?>
+
+<html>
+
+    <head>
+	<meta charset="utf-8">
+        <title>Formulário do Fornecedor</title>
+	<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../assets/css/all.css">
+        <script type="text/javascript" src="../assets/js/jquery-3.3.1.min.js"></script>
+        
+    </head>
+
+    <body>
+
+	<div class = "container-fluid" id="div-main">
+            
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#"> CIÊNCIA DA COMPUTAÇÃO - 7 CIC</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navForn" aria-controls="navForn" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            
+                <div class="collapse navbar-collapse" id="navForn">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Produtos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Fornecedores</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Categorias</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Clientes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Funcionários</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        
+            <hr>
+            
+            <div class="row">
+                <div class="col-md-12" style="text-align: center">
+                     <h2>Formulário de Fornecedor</h2>
+                </div>               
+            </div>
+            
+            <hr>
+            
+            <?php 
+                
+                // FORMULÁRIO VAZIO
+                if ($dados == 0 || $dados == null) {
+            
+            ?>
+            
+            <form method="GET" action="">
+                <div class="form-row">
+                    <div class="form-group col-md-4">
+                        <label for="company_forn">Nome da Companhia</label>
+                        <input id="company_forn" name="company_forn" type="text" class="form-control">
+                    </div>
+                    
+                    <div class="form-group col-md-4">
+                        <label for="contato_forn">Nome do Contato</label>
+                        <input id="contato_forn" name="contato_forn" type="text" class="form-control">
+                    </div>
+                    
+                    <div class="form-group col-md-4">
+                        <label for="titulo_forn">Título do Contato</label>
+                        <input id="titulo_forn" name="titulo_forn" type="text" class="form-control">
+                    </div>
+                </div> 
+                
+                <div class="form-row">
+                    <div class="form-group col-md-4">
+                        <label for="endereco_forn">Endereço</label>
+                        <input id="endereco_forn" name="endereco_forn" type="text" class="form-control">
+                    </div>
+                    
+                    <div class="form-group col-md-4">
+                        <label for="cidade_forn">Cidade</label>
+                        <input id="cidade_forn" name="cidade_forn" type="text" class="form-control">
+                    </div>
+                    
+                    <div class="form-group col-md-4">
+                        <label for="regiao_forn">Região</label>
+                        <input id="regiao_forn" name="regiao_forn" type="text" class="form-control">
+                    </div>
+                </div>         
+                
+                <div class="form-row">
+                    <div class="form-group col-md-4">
+                        <label for="postal_forn">Caixa Postal</label>
+                        <input id="postal_forn" name="postal_forn" type="text" class="form-control">
+                    </div>
+                    
+                    <div class="form-group col-md-4">
+                        <label for="estado_forn">País</label>
+                        <input id="estado_forn" name="pais_forn" type="text" class="form-control">
+                    </div>
+                    
+                    <div class="form-group col-md-4">
+                        <label for="tel_forn">Telefone</label>
+                        <input id="tel_forn" name="tel_forn" type="text" class="form-control">
+                    </div>
+                </div>                 
+                                
+                <hr>
+                
+                <div class="form-row">
+                    <div class="form-group col-md-12" style="text-align: left">
+                        <input type="hidden" name="acao" value="1">
+                        <button class="btn btn-success" type="submit">
+                            Cadastrar Fornecedor
+                            <i class="fas fa-save" style="margin-left: 5px"></i></button>
+                        
+                    </div>
+                </div>
+            </form>
+            
+            <?php 
+                
+                // FORMULÁRIO PREENCHIDO
+                } else if ($dados == 1) {
+                    if (isset($_GET['id'])) {
+                       $idForn = $_GET['id'];
+                        $objForn = $fornDAO->visualizarFornecedor($idForn); 
+                    
+                    } else {
+                        $objForn = NULL;
+                    }
+                    
+                    
+                    if ($objForn == NULL) {
+            
+            ?>
+            
+            <h2><center>FORNECEDOR NÃO ENCONTRADO!<i class="far fa-frown" style="margin-left: 10px"></i></center></h2>
+             
+            <?php } else { ?>
+            
+            <form method="GET" action="">
+                <div class="form-row">
+                    <div class="form-group col-md-4">
+                        <label for="company_forn">Nome da Companhia</label>
+                        <input id="company_forn" name="company_forn" type="text" class="form-control" value="<?= $objForn->getCompanyName() ?>">
+                    </div>
+                    
+                    <div class="form-group col-md-4">
+                        <label for="contato_forn">Nome do Contato</label>
+                        <input id="contato_forn" name="contato_forn" type="text" class="form-control" value="<?= $objForn->getContactName() ?>">
+                    </div>
+                    
+                    <div class="form-group col-md-4">
+                        <label for="titulo_forn">Título do Contato</label>
+                        <input id="titulo_forn" name="titulo_forn" type="text" class="form-control" value="<?= $objForn->getContactTitle() ?>">
+                    </div>
+                </div> 
+                
+                <div class="form-row">
+                    <div class="form-group col-md-4">
+                        <label for="endereco_forn">Endereço</label>
+                        <input id="endereco_forn" name="endereco_forn" type="text" class="form-control" value="<?= $objForn->getAddress() ?>">
+                    </div>
+                    
+                    <div class="form-group col-md-4">
+                        <label for="cidade_forn">Cidade</label>
+                        <input id="cidade_forn" name="cidade_forn" type="text" class="form-control" value="<?= $objForn->getCity() ?>">
+                    </div>
+                    
+                    <div class="form-group col-md-4">
+                        <label for="regiao_forn">Região</label>
+                        <input id="regiao_forn" name="regiao_forn" type="text" class="form-control" value="<?= $objForn->getRegion() ?>">
+                    </div>
+                </div>         
+                
+                <div class="form-row">
+                    <div class="form-group col-md-4">
+                        <label for="postal_forn">Caixa Postal</label>
+                        <input id="postal_forn" name="postal_forn" type="text" class="form-control" value="<?= $objForn->getPostalCode() ?>">
+                    </div>
+                    
+                    <div class="form-group col-md-4">
+                        <label for="estado_forn">País</label>
+                        <input id="estado_forn" name="pais_forn" type="text" class="form-control" value="<?= $objForn->getCountry() ?>">
+                    </div>
+                    
+                    <div class="form-group col-md-4">
+                        <label for="tel_forn">Telefone</label>
+                        <input id="tel_forn" name="tel_forn" type="text" class="form-control" value="<?= $objForn->getPhone() ?>">
+                    </div>
+                </div>                       
+    
+                <hr>
+                
+                <div class="form-row">
+                    <div class="form-group col-md-12" style="text-align: left">
+                        <input type="hidden" name="acao" value="2">
+                        <input type="hidden" name="id" value="<?= $idForn ?>">
+                        <button class="btn btn-info" type="submit">
+                            Alterar Fornecedor
+                            <i class="fas fa-pencil-alt" style="margin-left: 5px"></i></button>
+                        
+                    </div>
+                </div>
+            </form>
+            
+            <?php 
+                    }
+                }
+            
+            ?>
+            
+        </div>
+        
+    </body>
+    
+</html>
+
+<?php 
+
+    } 
+?>
+
