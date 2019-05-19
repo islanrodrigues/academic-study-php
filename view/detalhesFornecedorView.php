8ce69fba7100acdc2545c83a7d005976b703a7a3
Islan Rodrigues <jose-islan52@hotmail.com>
2019-05-18 23:48:14 -0300

    Ajustes nas telas de detalhes - aplicando links de navegação nos itens do menu

--- view/detalhesFornecedorView.php
+++ view/detalhesFornecedorView.php
@@ -63,16 +63,16 @@
                 <div class="collapse navbar-collapse" id="navForn">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Produtos</a>
+                            <a class="nav-link" href="listarProdutosView.php">Produtos</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Fornecedores</a>
+                            <a class="nav-link" href="listarFornecedoresView.php">Fornecedores</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Categorias</a>
+                            <a class="nav-link" href="listarCategoriasView.php">Categorias</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Funcionários</a>
+                            <a class="nav-link" href="listarFuncionariosView.php">Funcionários</a>
                         </li>
                     </ul>
                 </div>
